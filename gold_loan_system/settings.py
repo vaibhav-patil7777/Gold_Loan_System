b0213b8c9ac821a82f099c648066a1040c1a04a5
vaibhav Patil <vp14541610@gmail.com>
2025-10-31 15:54:27 +0530

    Configure Whitenoise for static file storage
    Added Whitenoise for static file compression and caching.

--- gold_loan_system/settings.py
+++ gold_loan_system/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+import dj_database_url
+
 
 from pathlib import Path
 
@@ -126,6 +129,8 @@ STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+# compress/static caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
